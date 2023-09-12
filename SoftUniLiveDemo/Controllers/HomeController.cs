3ef66c54a83ceb54e9e9d35e288a42b393d9fced
DenisBG312 <113765690+DenisBG312@users.noreply.github.com>
2023-09-12 12:50:45 +0300

    Add project files.

---
+++ SoftUniLiveDemo/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoftUniLiveDemo.Models;
+using System.Diagnostics;
+
+namespace SoftUniLiveDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Feedback()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Calculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calculator(double firstNumber, string operation, double secondNumber)
+        {
+            string result = string.Empty;
+            switch (operation)
+            {
+                case "+":
+                    result = $"{firstNumber + secondNumber}";
+                    break;
+                case "-":
+                    result = $"{firstNumber - secondNumber}";
+                    break;
+                case "*":
+                    result = $"{firstNumber * secondNumber}";
+                    break;
+                case "/":
+                    result = $"{firstNumber / secondNumber}";
+                    break;
+                default:
+                    result = "Invalid operation";
+                    break;
+            }
+
+            ViewBag.Result = result;
+            return View();
+        }
+    }
+}
